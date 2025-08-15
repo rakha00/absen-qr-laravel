b193ff0267eb804737c7d87ff331f9d7621a8087
Rakha <rkhdinar00@gmail.com>
2025-08-16 01:26:11 +0700

    refactor: remove email verification from user model and factory
    The email verification feature is no longer needed in the application, so we're removing related fields and methods from both the User model and UserFactory.

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,7 +51,6 @@ class User extends Authenticatable
     protected function casts(): array
     {
         return [
-            'email_verified_at' => 'datetime',
             'password' => 'hashed',
         ];
     }
